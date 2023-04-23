6d962867a3c6339afaf618b8fddf1ae8783b2212
aarotukia <tukiaaro@gmail.com>
2023-04-24 00:02:51 +0300

    fixed date formatting bug

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -93,12 +93,13 @@ async function getPrices(req, res) {
     }
 }
 // This function formats a date object into a string in the format "dd/mm/yy hh:mm"
-  function formatDate(date) {
+function formatDate(date) {
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const year = date.getFullYear().toString().slice(-2);
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
-    return `\${day}/\${month}/\${year} \${hours}:\${minutes}`;
-  }
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+}
+
 exports.getPrices = getPrices;

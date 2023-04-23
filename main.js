94b6359006e2c80e1af68035aaa7643fc6b0373c
Elias Westerling <elias.westerling@gmail.com>
2023-04-24 01:16:09 +0300

    fixed delete

--- main.js
+++ main.js
@@ -157,6 +157,7 @@ router.post("/users/create", usersController.create, usersController.redirectVie
 router.get("/users/:id", usersController.show, usersController.showView);
 router.get("/users/:id/edit", usersController.edit);
 router.put("/users/:id/update", usersController.update, usersController.redirectView);
+router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
 
 
 

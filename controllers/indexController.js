f1ef573328037b290a9e01158f66a87bb1bf6017
aarotukia <tukiaaro@gmail.com>
2023-04-23 18:07:44 +0300

    Changes to date formatting and price display

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -8,17 +8,33 @@ async function fetchLatestPriceData() {
   return response.json();
 }
 
-function getPriceForDate(date, prices) {
-  const matchingPriceEntry = prices.find(
-    (price) => new Date(price.startDate) <= date && new Date(price.endDate) > date
-  );
-
-  if (!matchingPriceEntry) {
-    throw 'Price for the requested date is missing';
+function formatDateTime(date) {
+    return date.toLocaleString('en-GB', {
+      day: '2-digit',
+      month: '2-digit',
+      year: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit'
+    }).replace(',', '');
   }
 
-  return matchingPriceEntry.price;
-}
+  
+function getPriceForDate(date, prices) {
+    const matchingPriceEntry = prices.find(
+      (price) => new Date(price.startDate) <= date && new Date(price.endDate) > date
+    );
+  
+    if (!matchingPriceEntry) {
+      throw 'Price for the requested date is missing';
+    }
+  
+    const formattedDate = formatDateTime(date);
+    const price = matchingPriceEntry.price;
+  
+    return `${formattedDate}: ${price}`;
+  }
+  
+  
 
 exports.index = async (req, res) => {
   const { prices } = await fetchLatestPriceData();
